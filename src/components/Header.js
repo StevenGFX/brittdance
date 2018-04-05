d1e5a09ce7254aa835244efd4d2e770e53aca118
Steven Williams <me@stevengfx.com>
2018-04-05 00:25:11 -0500

    Initial commit

---
+++ src/components/Header.js
@@ -0,0 +1,30 @@
+import React from 'react'
+
+const Header = (props) => (
+    <header id="header" style={props.timeout ? {display: 'none'} : {}}>
+        <div className="logo">
+            <span className="icon fa-female"></span>
+        </div>
+        <div className="content">
+            <div className="inner">
+                <h1>Brittney Brewer</h1>
+                <p>This is a little space for a short teaser text content</p>
+            </div>
+        </div>
+        <nav>
+            <ul>
+                <li><a href="javascript:;" onClick={() => {props.onOpenArticle('intro')}}>Philosophy</a></li>
+                <li><a href="javascript:;" onClick={() => {props.onOpenArticle('work')}}>Work</a></li>
+                <li><a href="javascript:;" onClick={() => {props.onOpenArticle('about')}}>About</a></li>
+                <li><a href="javascript:;" onClick={() => {props.onOpenArticle('contact')}}>Contact</a></li>
+            </ul>
+        </nav>
+    </header>
+)
+
+Header.propTypes = {
+    onOpenArticle: React.PropTypes.func,
+    timeout: React.PropTypes.bool
+}
+
+export default Header
